cf8297278bb28a01c72f72bc07bc508f76533281
youcef <youcef.boumezaar@univ-constantine2.dz>
2023-08-03 12:31:43 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+
+void main() {
+  runApp(micard());
+}
+
+class micard extends StatelessWidget {
+  const micard({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp    (
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/20221208_130207.jpg'),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+213 657759496',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'Youcef Boumezaar',
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+
+
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+
+  }
+}
